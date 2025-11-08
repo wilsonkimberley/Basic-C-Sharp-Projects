2f458b9cd75ef17f7c08274485151a7dd609adb0
wilsonkimberley <kimberleyannuk@gmail.com>
2025-11-08 20:54:00 +0000

    Writing and Running Code in C# Assignment

---
+++ AcmeAccountingSystem/AcmeAccountingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;  // Gives access to basic system functions like Console
+
+class Program
+{
+    static void Main()
+    {
+        // Display the welcome message
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+
+        // Display the second line with the quote
+        Console.WriteLine("Remember, we’re \"accounting\" on you!");
+    }
+}
