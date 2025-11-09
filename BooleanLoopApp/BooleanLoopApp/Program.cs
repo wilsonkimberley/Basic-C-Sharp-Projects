e15ba6d134c24857990a1d49b4e905f1963ae2f8
wilsonkimberley <kimberleyannuk@gmail.com>
2025-11-09 00:15:41 +0000

    Console App Assignment

---
+++ BooleanLoopApp/BooleanLoopApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class BooleanLoopApp
+{
+    static void Main()
+    {
+        // WHILE LOOP
+        int counter = 1;
+        bool keepRunning = true;
+
+        while (keepRunning)
+        {
+            Console.WriteLine("While loop iteration: " + counter);
+            counter++;
+
+            if (counter > 5)
+            {
+                keepRunning = false;
+            }
+        }
+
+        // DO-WHILE LOOP
+        int doCounter = 1;
+        bool continueLoop = true;
+
+        do
+        {
+            Console.WriteLine("Do-While loop iteration: " + doCounter);
+            doCounter++;
+
+            if (doCounter > 3)
+            {
+                continueLoop = false;
+            }
+        } while (continueLoop);
+    }
+}
